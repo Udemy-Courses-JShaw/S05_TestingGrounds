283fc44e8a681acead54ab64327f0285cf90de70
Jarrod G Shaw <jarrod.g.shaw@gmail.com>
2020-01-23 12:10:26 -0800

    Initial Commit!!

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # S05_TestingGrounds
+A FPS shooter with More Advanced AI, Procedurally generated Terrain, pickups, Skeletal Meshes, all using the Unreal engine 4.
---
+++ Source/S05_TestingGroundsEditor.Target.cs
@@ -0,0 +1,14 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class S05_TestingGroundsEditorTarget : TargetRules
+{
+	public S05_TestingGroundsEditorTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		ExtraModuleNames.Add("S05_TestingGrounds");
+	}
+}
